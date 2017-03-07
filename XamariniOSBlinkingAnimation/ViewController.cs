7d87f2abc6580ec07b3fcd14754f914e4063b624
DelpinSusaiRaj <susairajs@outlook.com>
2017-03-07 16:19:08 +0530

    add all files

---
+++ XamariniOSBlinkingAnimation/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UIKit;
+
+
+using System.Timers;
+
+namespace XamariniOSBlinkingAnimation
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+		}
+				public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			Timer timer = new Timer();
+
+			timer.Interval = 500;
+			timer.Start();
+			timer.Elapsed += (object sender, ElapsedEventArgs e) =>
+			{
+				InvokeOnMainThread(() =>
+				{
+					if (lblBlink.TextColor == UIColor.Black)
+
+						lblBlink.TextColor = UIColor.Red;
+
+					else
+
+						lblBlink.TextColor = UIColor.Black;
+
+					if (lblAndroid.TextColor == UIColor.Black)
+
+						lblAndroid.TextColor = UIColor.White;
+
+					else
+
+						lblAndroid.TextColor = UIColor.Black;
+
+				});
+
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
